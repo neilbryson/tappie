627212bf7e1cb8d720244166ae203f588536b040
Neil Bryson Cargamento <neilbrysonmc@gmail.com>
2022-06-17 22:28:03 +0800

    implement simple ui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
-fn main() {
-    println!("Hello world!");
+use anyhow::Result;
+use crossterm::{
+    event::{self, poll, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::io;
+use std::thread;
+use std::time::{Duration, Instant};
+use tui::{
+    backend::{Backend, CrosstermBackend},
+    layout::{Constraint, Direction, Layout},
+    widgets::{Block, Borders, Widget},
+    Frame, Terminal,
+};
+
+fn main() -> Result<(), io::Error> {
+    enable_raw_mode()?;
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.clear()?;
+    terminal.hide_cursor()?;
+
+    let tick_rate = Duration::from_millis(300);
+    let mut last_tick = Instant::now();
+    let mut should_quit = false;
+
+    loop {
+        let timeout = tick_rate
+            .checked_sub(last_tick.elapsed())
+            .unwrap_or_else(|| Duration::from_secs(0));
+
+        terminal.draw(|f| {
+            let size = f.size();
+            let block = Block::default().title("tappie").borders(Borders::ALL);
+            f.render_widget(block, size);
+        })?;
+
+        if poll(timeout)? {
+            if let Event::Key(key) = event::read()? {
+                match key.code {
+                    KeyCode::Char('q') => should_quit = true,
+                    _ => {}
+                }
+            }
+        }
+
+        if last_tick.elapsed() >= tick_rate {
+            last_tick = Instant::now();
+        }
+
+        if should_quit {
+            return Ok(());
+        }
+    }
 }
